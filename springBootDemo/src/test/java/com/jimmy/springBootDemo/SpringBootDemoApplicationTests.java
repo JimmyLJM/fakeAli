c08f38a3508cf01d3d342a13f2a0c3d61c6bbf0d
JimmyLJM <ljm506937617@163.com>
2017-09-16 08:16:37 +0800

    测试test

--- springBootDemo/src/test/java/com/jimmy/springBootDemo/SpringBootDemoApplicationTests.java
+++ springBootDemo/src/test/java/com/jimmy/springBootDemo/SpringBootDemoApplicationTests.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 import java.util.Date;
 
 @RunWith(SpringRunner.class)
@@ -39,6 +44,22 @@ public class SpringBootDemoApplicationTests {
 
 	}
 
+	@Test
+	public void sendAttachmentsEmail() throws MessagingException {
+		MimeMessage mimeMessage =  mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+		//基本设置.
+		helper.setFrom("jimmycoding@sina.com");//发送者.
+		helper.setTo("jimmycoding@sina.com");//接收者.
+		helper.setSubject("测试附件（邮件主题）");//邮件主题.
+		helper.setText("这是邮件内容（有附件哦.）");//邮件内容.
+
+		FileSystemResource file1 = new FileSystemResource(new File("D:/87097.jpg"));
+
+		helper.addAttachment("附件.jpg", file1);
+		mailSender.send(mimeMessage);
+	}
 
 
 }
